744f6eef1fce149b64d74f3324e51a94d49c9c8c
nawa_NotePC <nawa.scraping@gmail.com>
2022-01-27 22:54:34 +0900

    なかなか難航している

--- inputImgHandler.js
+++ inputImgHandler.js
@@ -1,19 +1,30 @@
+
+const fileReader = new FileReader();
+
 //テストとしてテキストを読んでみる
-
-function inputChange() {
-    let files = imgFile.files;
-    let fileReader = new FileReader();
-
-    fileReader.addEventListener('load', function(e) {
-        console.log(e.target.result);
-    });
-    
+inputChange = () => {
+    const files = imgFile.files;
     //バッファを確保、その後2バイトでtag読み、あとは変調してバイト読み込み
-    fileReader.readAsArrayBuffer(files[0]);
+    fileReader.readAsArrayBuffer(files[0]);//ここの反応によっていろんなイベントが発生する
+    //↑のイベントの一部、ファイル読み込み後に発火するonload(もしくは'load')を使用。
+    fileReader.addEventListener('load', function(e) {
+        const dataViewer = new DataView(e.target.result)
+        console.log(dataViewer);
+
+        const readButton = document.getElementById('readButton');
+        let pushCounter = 0;
+        readButton.addEventListener('click', function (e) {
+            console.log(dataViewer.getUint16(pushCounter).toString(16))
+            pushCounter++
+            console.log(pushCounter);
+        });
+    })
 
 }
 
-let imgFile = document.getElementById('imgFile');
+
+
+const imgFile = document.getElementById('imgFile');
 imgFile.addEventListener('change', inputChange);
 
 
